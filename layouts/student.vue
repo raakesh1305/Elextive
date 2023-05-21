09ef6eef846dc37ac08ce8783be454dea5a3790d
M.RAAKESH <84133959+raakesh1305@users.noreply.github.com>
2023-05-21 18:23:03 +0530

    Add files via upload

---
+++ layouts/student.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Nav/>
+    <div class="has-navbar-fixed-top has-navbar-fixed-bottom">
+      <div class="pad-top">
+      <nuxt />
+      </div>
+    </div>
+    <Bottomnav/>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import Nav from '~/components/Nav.vue'
+import Bottomnav from '~/components/Bottomnav.vue'
+export default {
+  components: {
+    Nav,
+    Bottomnav
+  }
+}
+</script>
+
+<style scoped>
+.pad-top {
+  padding-top: 3.5rem;
+}
+</style>
